eff4e7dda87133c09edb9d74242ba5d76dee0775
ahmetveburak <ahmetbozyurtt@gmail.com>
2022-09-17 19:41:25 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+__version__ = "0.1.0"
+
+setuptools.setup(
+    name="django-bulk-copy",
+    version=__version__,
+    author="Ahmet Burak",
+    author_email="ahmetbozyurtt@gmail.com",
+    license="MIT",
+    description="Write models to db with copy command.",
+    url="https://github.com/ahmetveburak/django-bulk-copy",
+    python_requires=">=3.6",
+    install_requires=["django>=3.2"],
+    packages=setuptools.find_packages(),
+)
