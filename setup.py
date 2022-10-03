f90a68afd26e7586fddd2e6a2b9f6528df3272d3
Ahmet Burak <31131727+ahmetveburak@users.noreply.github.com>
2022-10-04 00:06:59 +0300

    v0.1.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 
 setuptools.setup(
     name="django-bulk-copy",
@@ -12,5 +12,6 @@ setuptools.setup(
     url="https://github.com/ahmetveburak/django-bulk-copy",
     python_requires=">=3.6",
     install_requires=["django>=3.2"],
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=(("tests",))),
+    include_package_data=True,
 )
